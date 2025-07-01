a064c1cd6773f6854aa785a429042275e5f87d8b
azib-ep <azib@empowerplus.my>
2025-07-01 22:26:06 +0800

    added tailwindcss and daisyui for styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
 // import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  }
   // ssr
   // output: "server",
   // adapter: node({
