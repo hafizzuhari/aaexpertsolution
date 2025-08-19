84d96c4cda745b2955f233557d4a993287f31b7b
azib-af <azibahmadfadzli@gmail.com>
2025-08-19 10:22:57 +0800

    feat: added certification section and quotation form

--- src/content/2.1-certifications.ts
+++ src/content/2.1-certifications.ts
@@ -1,8 +1,19 @@
 export const certifications = {
   title: "Pensijilan dan Akreditasi",
+  description:
+    "Kami komited untuk memastikan setiap projek dijalankan mengikut piawaian tertinggi industri. Dengan pensijilan rasmi dan pematuhan kepada badan berautoriti, kami memberi jaminan kualiti, keselamatan, dan kebolehpercayaan kepada setiap pelanggan.",
   items: [
-    "CIDB Registered Contractor",
-    "Certified Welders & Fabricators",
-    "Compliant with JKR & Local Council Standards"
-  ]
-}
+    {
+      icon: "certificate",
+      text: "Kontraktor Berdaftar CIDB",
+    },
+    {
+      icon: "certificate-2",
+      text: "Pekerja Berkimpal & Fabrikasi Bertauliah",
+    },
+    {
+      icon: "file-certificate",
+      text: "Pematuhan Standard JKR & Pihak Berkuasa Tempatan",
+    },
+  ],
+};
