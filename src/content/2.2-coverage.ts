dc54a10b3506549df28bac00b3e1be05344129b4
azib-af <azibahmadfadzli@gmail.com>
2025-08-21 01:24:12 +0800

    feat: added coverage with map

--- src/content/2.2-coverage.ts
+++ src/content/2.2-coverage.ts
@@ -1,5 +1,42 @@
 export const coverageArea = {
   title: "Kawasan Kami",
-  description: "Kami menyediakan servis kami ke kawasan sekitar Selangor, Kuala Lumpur sehingga ke Perak dan Negeri Sembilan.",
-  locations: ["Kajang", "Cheras", "Bangi", "Shah Alam", "Putrajaya", "Serdang", "Semenyih"]
+  description:
+    "Kami menyediakan servis kami ke kawasan sekitar Selangor, Kuala Lumpur sehingga ke Perak dan Negeri Sembilan.",
+  locations: [
+    {
+      name: "Kajang",
+      state: "Selangor",
+      coordinates: { lat: 2.9935, lng: 101.7871 }
+    },
+    {
+      name: "Cheras",
+      state: "Kuala Lumpur",
+      coordinates: { lat: 3.0463, lng: 101.7405 }
+    },
+    {
+      name: "Bangi",
+      state: "Selangor",
+      coordinates: { lat: 2.9608, lng: 101.7606 }
+    },
+    {
+      name: "Shah Alam",
+      state: "Selangor",
+      coordinates: { lat: 3.0738, lng: 101.5183 }
+    },
+    {
+      name: "Putrajaya",
+      state: "Wilayah Persekutuan Putrajaya",
+      coordinates: { lat: 2.9353, lng: 101.6911 }
+    },
+    {
+      name: "Serdang",
+      state: "Selangor",
+      coordinates: { lat: 3.025, lng: 101.705 }
+    },
+    {
+      name: "Semenyih",
+      state: "Selangor",
+      coordinates: { lat: 2.9517, lng: 101.8432 }
+    }
+  ]
 }
